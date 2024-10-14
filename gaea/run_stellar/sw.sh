6f4b3c6cc874ec133bfaba714b6248102ae35bd5
luanfs <luanfonseca93@gmail.com>
2024-10-14 13:58:59 -0400

    Refactor run and compilation scripts for better organization

--- gaea/run_stellar/sw.sh
+++ gaea/run_stellar/sw.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Luan Santos - 2023
-# Script to run advection/sw test cases for different parameters
+# Script to run sw test cases for different parameters
 
 
 #----------------------------------------------------------------------------------------------
@@ -8,7 +8,7 @@
 COMP="repro"      # choices:  debug, repro, prod
 #COMP="debug"
 clear
-./compile.sh $COMP noclean
+./compile.sh $COMP
 
 #----------------------------------------------------------------------------------------------
 # Modify this part for your purposes
@@ -16,50 +16,57 @@ clear
 # plot_scalar_field.py, plot_converge.py and plot_error_graph.py
 
 #test case
-tc="-6"
+tc="2"
 
 # 1d advection scheme
-hords=(0 8)
-#hords=(8)
+#hords=(5 8)
+hords=(8)
 
 # 2d advection scheme
-#advs=(1 2 1 2 1 2 1 2)
-advs=(1 2 1 2)
-#advs=(2)
+#advs=(1 1 1 1 1 1)
+advs=(1 1 2 2)
+#advs=(1 2)
+#advs=(1 1 1 1)
+#advs=(2 2 2 2)
+#advs=(2 2)
+#advs=(1)
 
 # Simulation arrays
 #grid_type (0-equiedge; 2-equiangular)
-#gtypes=(2 2 0 0 2 2 0 0)
-gtypes=(2 2 0 0)
-#gtypes=(0 2 0 2)
-#gtypes=(2 0)
-#gtypes=(0 0 0)
 #gtypes=(2)
-
-#mass fixer
-#mfs=(1 1 1 1 0 0 0 0)
-mfs=(1 1 1 1) #always true
-
-#midpoint cube
-#mps=(".false." ".false." ".true.")
-mps=(".false." ".false." ".false." ".false." ".false." ".false." ".false." ".false." ".false." ".false." ".false." ".false.")
+#gtypes=(2 2 2 0 0 0)
+gtypes=(0 2 0 2)
+#gtypes=(0 0 2 2)
+#gtypes=(2 2 2 2 2 2)
+#gtypes=(2 2)
+#gtypes=(0 2)
+#gtypes=(2)
+#gtypes=(0 0 0 0)
+#gtypes=(2 2 2 2)
 
 # duogrid schemes (0-none; 1-interpolation based on geodesic distances; 2-interpolation base on cube distances)
-dgs=(2 2 2 2 2 2 2 2)
-#dgs=(1 1 2 2)
-#dgs=(0 2 2)
-#dgs=(2 2)
+dgs=(2 2 2 2 2 2)
+#dgs=(1 1 1 1 1 1)
+#dgs=(0 0 0 0 0 0)
+
+#mass fixer
+mfs=(1 1 1 1)
+#mfs=(0 1 0 1)
 
 #divergence damping (only for sw)
 # variables for hyperthreading
-if [ $tc = "2"  ]; then
+if [ $tc = "2" ]; then
    dds=(0.12 0.12 0.12 0.12 0.12 0.12 0.12 0.12 0.12 0.12 0.12 0.12)
 else
-   dds=(0 0 0 0 0 0 0 0 0 0 0 0)
+   dds=(0.12 0.12 0.12 0.12 0.12 0.12 0.12 0.12 0.12 0.12 0.12 0.12)
+   #dds=(0 0 0 0 0 0 0 0 0 0)
+   #dds=(0 0.12 0 0.12)
 fi
+#dds=(0 0.12 0 0.12)
 
 # rotation angles
 alphas=(45 45 45 45 45 45 45 45)
+#alphas=(0 0 0 0 0 0)
 
 # Initial value of N
 N=48
@@ -71,76 +78,40 @@ Ng=4
 
 
 case $tc in
-    #------------- advection tests-------------
-    1)
-    tcname="cosine-zonal"
-    days="12"
-    dt=3600
-    n_split=1
-    ;;
-
-    -3)
-    tcname="gaussian-zonal"
-    days="12"
-    dt=3600
-    n_split=1
-    ;;
-
-    -4)
+    #-------------SW tests-------------
+    2)
     tcname="geobalance"
-    days="12"
+    days="3"
     dt=3600
-    n_split=1
+    n_split=7
     ;;
 
-    -5)
-    tcname="twogaussians-ndiv"
-    days="12"
-    dt=1600
-    n_split=1
+    5)
+    tcname="mountain"
+    days="14"
+    dt=1800
+    n_split=7
     ;;
 
-    -6)
-    tcname="twogaussians-div"
-    days="12"
-    dt=6400
-    #dt=2592
-    n_split=1
+    6)
+    tcname="rhwave"
+    days="105"
+    dt=1200
+    n_split=0
     ;;
 
-    -7)
-    tcname="cylinder-zonal"
-    days="12"
-    dt=3600
-    n_split=1
+    7)
+    tcname="galewski"
+    days="8"
+    dt=1800
+    n_split=8
     ;;
 
-    -8)
-    tcname="cylinder-ndiv"
-    days="12"
-    dt=1600
-    n_split=1
-    ;;
-
-    -9)
-    tcname="cylinder-div"
-    days="12"
-    dt=6400
-    n_split=1
-    ;;
- 
-    -2)
-    tcname="duogrid-test"
-    days="12"
-    dt=3600
-    n_split=1
-    ;;
-
-    -10)
-    tcname="recon-test"
-    days="12"
-    dt=3600
-    n_split=1
+    110)
+    tcname="divwind"
+    days="7"
+    dt=3200
+    n_split=7
     ;;
 esac
 
@@ -167,14 +138,13 @@ for ((j=1; j<=$Ng; j++)); do
             dd=${dds[i]}
             adv=${advs[i]}
             mf=${mfs[i]}
-   
-            if [[ $tc == -6 || $tc == -5  || $tc == -8 || $tc == -9 ]]; then
+ 
+            if [[ $tc -ne 1 && $tc -ne 2 ]]; then
                 alpha=0
             fi
             # Run the code shallow water script
-            echo N$N g$gtype dg$dg a$alpha  tc$tc dt$dt
             sbatch ./fv3_sw.csh $tc $N $dt $gtype $dg $tcname $alpha $days $hours $minutes $seconds $COMP $n_split $hord $dd $adv $mf
-
+            #echo N$N g$gtype dg$dg a$alpha dt$dt tc$tc
             #sleep 10
         done
     done
