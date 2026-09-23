05ff4b0fcf9cdf6567de64c2a7548d05a602d23c
luanfs <luanfsantos14@gmail.com>
2026-09-23 09:00:49 -0300

    clean sh dir

--- sh/untar.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Untar tarball script
-cd ..
-tar -xvf container_duo.tar.bz2
--- sh/ybytu.sh
+++
@@ -1,2 +0,0 @@
-# Script to log at usp cluster
-ssh -CYttg -L 10000:localhost:10000 luansantos@ime.usp.br ssh -CYtt -L 10000:localhost:22 luansantos@ybytu
